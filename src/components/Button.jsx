868208ef69025f4e4260e621747e3a1bba9b31bd
Phil Mass <philrmass@gmail.com>
2019-03-09 17:23:50 -0800

    connect file input to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.css';
 
-function Button({children}) {
+function Button({ onClick, children }) {
   return (
-    <button className={styles.button}>
+    <button 
+      className={styles.button}
+      onClick={onClick}>
       {children}
     </button>
   );
 }
 
 Button.propTypes = {
+  onClick: PropTypes.func,
   children: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element
--- src/components/SourceLoadForm.jsx
+++ src/components/SourceLoadForm.jsx
@@ -5,13 +5,57 @@ import styles from './SourceLoadForm.css';
 class SourceLoadForm extends React.Component {
   constructor(props) {
     super(props);
+    this.handleFileClick = this.handleFileClick.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleFileSubmit = this.handleFileSubmit.bind(this);
+    //this.handleUrlSubmit = this.handleUrlSubmit.bind(this);
+    this.fileInput = React.createRef();
   }
 
+  handleFileClick(event) {
+    event.preventDefault();
+    this.fileInput.current.click();
+    console.log('file click', this.fileInput.current);
+  }
+
+  handleFileChange(event) {
+    event.preventDefault();
+    console.log('file change', this.fileInput.current.files);
+  }
+  /*
+  handleChange(event) {
+    event.preventDefault();
+    console.log('change');
+  }
+
+  handleFileSubmit(event) {
+    event.preventDefault();
+    console.log('submit', this.fileInput.current.files[0]);
+  }
+
+  handleUrlSubmit(event) {
+    event.preventDefault();
+    console.log('submit', this.fileInput);
+  }
+
+*/
   render() {
     return (
       <div className={styles.sourceLoadForm}>
+        <Button onClick={this.handleFileClick}>
+          Click
+        </Button>
+        <span>or drop a file here</span>
+        <input type='file' ref={this.fileInput} onChange={this.handleFileChange}/>
+      </div>
+    );
+  }
+}
+/*
+        <input id='input' type='file' ref={this.fileInput}/>
         <div>Load an image or website</div>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={this.handleFileSubmit}>
           <span className={styles.inputLabel}>
             Local File
           </span>
@@ -19,24 +63,23 @@ class SourceLoadForm extends React.Component {
             <span className={styles.input}>
               File Name Here
             </span>
-            <input id='input' type='file'/>
+            <input id='input' type='file' ref={this.fileInput}/>
           </label>
           <Button>
             Load
           </Button>
+        </form>
+        <form className={styles.form} onSubmit={this.handleUrlSubmit}>
           <label htmlFor='urlInput'>
             <span className={styles.inputLabel}>
               URL
             </span>
           </label>
-          <input id='urlInput' type='text' className={styles.input}/>
+          <input id='urlInput' type='text' className={styles.input} onChange={this.handleChange}/>
           <Button>
             Load
           </Button>
         </form>
-      </div>
-    );
-  }
-}
+        */
 
 export default SourceLoadForm;
