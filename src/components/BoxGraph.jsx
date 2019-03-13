4cccb23162e5c3f4ee876144654c8952851035bc
Phil Mass <philrmass@gmail.com>
2019-03-13 14:38:23 -0700

    add color to box graph, style box graph

--- src/components/BoxGraph.jsx
+++ src/components/BoxGraph.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './BoxGraph.css';
 
-function BoxGraph() {
+function BoxGraph({ colors }) {
   return (
     <div className={styles.boxGraph}>
-      <span>BoxGraph</span>
+      <div>{colors && colors.length} colors</div>
     </div>
   );
 }
+
+BoxGraph.propTypes = {
+  colors: PropTypes.array
+};
 
 export default BoxGraph;
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -5,17 +5,11 @@ import BoxGraph from './BoxGraph';
 import styles from './Graph.css';
 
 function Graph({ sources, url }) {
-  const current = sources[url];
-  const name = current && current.name;
-  const colors = current && current.colors && current.colors.length;
   return (
     <div className={styles.boxGraph}>
       <div style={{fontSize: '0.8em'}}>
-        <div>Sources: {Object.keys(sources).length}</div>
-        <div>{name}</div>
-        <div>{colors}</div>
       </div>
-      <BoxGraph/>
+      <BoxGraph colors={sources[url] && sources[url].colors} />
     </div>
   );
 }
