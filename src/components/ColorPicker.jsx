e5c0ca78b7fc45a8e1a2f6fc3b12efaa04d4795b
Phil Mass <philrmass@gmail.com>
2019-03-12 17:44:31 -0700

    change light with button

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { setColorHex, setColorHsl } from '../actions';
+import { setColorHsl } from '../actions';
 import CopyIcon from './CopyIcon';
 import Clipboard from '../clipboard';
 import styles from './ColorPicker.css';
 
-function updateLight(light, event) {
-  const scale = -0.05;
-  light = light + (scale * event.movementY);
-  light = (light < 0) ? 0 : light;
-  light = (light > 100) ? 100 : light;
-  return light;
+function updateLight(color, event, dispatch) {
+  if(event.buttons === 1) {
+    const scale = -0.05;
+    let light = color.light + (scale * event.movementY);
+    light = (light < 0) ? 0 : light;
+    light = (light > 100) ? 100 : light;
+    dispatch(setColorHsl(color.hue, color.sat, light));
+  }
 }
 
 function ColorPicker({ dispatch, color }) {
@@ -19,8 +21,7 @@ function ColorPicker({ dispatch, color }) {
   return (
     <div 
       className={styles.colorPicker}
-      style={style}
-      onClick={() => dispatch(setColorHex('6b2097'))}>
+      style={style}>
       <div 
         className={styles.hslaBar}
         style={style}>
@@ -34,7 +35,7 @@ function ColorPicker({ dispatch, color }) {
       <div className={styles.hueSatBox}>
         <div 
           className={styles.lightBox} 
-          onMouseMove={(event) => dispatch(setColorHsl(color.hue, color.sat, updateLight(color.light, event)))}>
+          onMouseMove={(event) => updateLight(color, event, dispatch)}>
         </div>
       </div>
     </div>
