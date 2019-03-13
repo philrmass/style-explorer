06a19dfdc4db8dd0c7271884c5175428d492e7d3
Phil Mass <philrmass@gmail.com>
2019-03-13 10:02:58 -0700

    adjust layout with columns

--- src/components/MainPane.jsx
+++ src/components/MainPane.jsx
@@ -12,30 +12,34 @@ import styles from './MainPane.css';
 function MainPane({ dispatch, ui }) {
   return (
     <div className={styles.mainPane}>
-      <Collapsible
-        name='Color Picker'
-        isOpen={ui.colorPickerIsOpen}
-        onClick={() => dispatch(toggleColorPicker())}>
-        <ColorPicker/>
-      </Collapsible>
-      <Collapsible
-        name='Graph'
-        isOpen={ui.graphIsOpen}
-        onClick={() => dispatch(toggleGraph())}>
-        <Graph/>
-      </Collapsible>
-      <Collapsible
-        name='Sources'
-        isOpen={ui.sourcesIsOpen}
-        onClick={() => dispatch(toggleSources())}>
-        <Sources/>
-      </Collapsible>
-      <Collapsible
-        name='Source'
-        isOpen={ui.sourceIsOpen}
-        onClick={() => dispatch(toggleSource())}>
-        <Source/>
-      </Collapsible>
+      <div className={styles.leftLayout}>
+        <Collapsible
+          name='Color Picker'
+          isOpen={ui.colorPickerIsOpen}
+          onClick={() => dispatch(toggleColorPicker())}>
+          <ColorPicker/>
+        </Collapsible>
+        <Collapsible
+          name='Graph'
+          isOpen={ui.graphIsOpen}
+          onClick={() => dispatch(toggleGraph())}>
+          <Graph/>
+        </Collapsible>
+      </div>
+      <div className={styles.rightLayout}>
+        <Collapsible
+          name='Sources'
+          isOpen={ui.sourcesIsOpen}
+          onClick={() => dispatch(toggleSources())}>
+          <Sources/>
+        </Collapsible>
+        <Collapsible
+          name='Source'
+          isOpen={ui.sourceIsOpen}
+          onClick={() => dispatch(toggleSource())}>
+          <Source/>
+        </Collapsible>
+      </div>
     </div>
   );
 }
